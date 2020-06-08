b8ad5d1497d7dc45321be2e268b073d65b6dfe38
juwilhel <64095825+juwilhel@users.noreply.github.com>
2020-06-08 09:08:05 +0200

    Update auxiliary_tables.py

--- auxiliary/auxiliary_tables.py
+++ auxiliary/auxiliary_tables.py
@@ -6,6 +6,7 @@ import matplotlib as plt
 import pandas as pd
 import numpy as np
 import statsmodels as sm
+import statsmodels.api as sm_api
 
 from auxiliary.example_project_auxiliary_predictions import *
 from auxiliary.example_project_auxiliary_plots import *
@@ -39,6 +40,45 @@ def create_table1(data):
     table1 = table11.append(table12).rename(index={"F": "Female", "M": "Male"}, columns={0.0: "Basic-Savings", 1.0: "Tertiary"})
     
     return table1
+
+def create_table2(data1, data2):
+    """
+      Creates Table 2.
+    """
+    x_sancristobal = data1[['T1_treat','T2_treat']]
+    x_sancristobal = sm_api.add_constant(x_sancristobal)
+    x_suba = data2['T3_treat']
+    x_suba = sm_api.add_constant(x_suba)
+    result_sancristobal = list()
+    result_suba = list()
+    T_Test = list()
+    Control_avg_bs = list()
+    Control_avg_t = list()
+    for i in ['s_teneviv_int','s_utilities','s_durables','s_infraest_hh','s_age_sorteo','s_sexo_int','s_yrs','s_single','s_edadhead','s_yrshead','s_tpersona','s_num18','s_estrato','s_puntaje','s_ingtotal']:
+        y_sancristobal = data1[i]
+        y_suba = data2[i]
+        reg_sancristobal = sm_api.OLS(y_sancristobal, x_sancristobal).fit(cov_type='cluster', cov_kwds={'groups': data1['school_code']})
+        result_sancristobal.append(round(reg_sancristobal.params, 2))
+        result_sancristobal.append(round(reg_sancristobal.bse, 2))
+        T_Test.append(round(reg_sancristobal.t_test('T1_treat=T2_treat').effect[0], 2))
+        T_Test.append(round(reg_sancristobal.t_test('T1_treat=T2_treat').sd[0][0], 2))
+        reg_suba = sm_api.OLS(y_suba, x_suba).fit(cov_type='cluster', cov_kwds={'groups': data2['school_code']})
+        result_suba.append(round(reg_suba.params, 2))
+        result_suba.append(round(reg_suba.bse, 2))
+        Control_avg_bs.append("%.2f" % round(data1.groupby(data1['control']).mean()[i][1], 2))
+        Control_avg_bs.append(round(data1.groupby(data1['control']).std()[i][1], 2))
+        Control_avg_t.append("%.2f" % round(data2.groupby(data2['control']).mean()[i][1], 2))
+        Control_avg_t.append(round(data2.groupby(data2['control']).std()[i][1], 2))
+    table21 = pd.DataFrame(result_sancristobal, index=['Possessions','Possessions SE','Utilities','Utilities SE','Durable Goods','Durable Goods SE','Physical Infrastructure','Physical Infrastructure SE','Age','Age SE','Gender','Gender SE','Years of Education','Years of Education SE','Single Head','Single Head SE','Age of Head','Age of Head SE','Years of ed., head','Years of ed., head SE','People in Household','People in Household SE','Member under 18','Member under 18 SE','Estrato','Estrato SE','SISBEN score','SISBEN score SE','Household income (1,000 pesos)','Household income (1,000 pesos) SE'])
+    table21.columns = ['Control average B-S', 'Basic-Control', 'Savings-Control']
+    table21['Control average B-S'] = Control_avg_bs
+    table21['Basic-Savings'] = T_Test
+    table22 = pd.DataFrame(result_suba, index=['Possessions','Possessions SE','Utilities','Utilities SE','Durable Goods','Durable Goods SE','Physical Infrastructure','Physical Infrastructure SE','Age','Age SE','Gender','Gender SE','Years of Education','Years of Education SE','Single Head','Single Head SE','Age of Head','Age of Head SE','Years of ed., head','Years of ed., head SE','People in Household','People in Household SE','Member under 18','Member under 18 SE','Estrato','Estrato SE','SISBEN score','SISBEN score SE','Household income (1,000 pesos)','Household income (1,000 pesos) SE'])
+    table22.columns = ['Control average T', 'Tertiary-Control']
+    table22['Control average T'] = Control_avg_t
+    table2 = table21.join(table22)
+    
+    return table2
 
 
 def create_table6(dictionary, keys, regressors):
